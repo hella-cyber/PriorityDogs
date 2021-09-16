f29112e3499f408a4db9b4ea2170ffc3737eb51b
hella de ridder <hella.deridder.bee@outlook.com>
2021-09-16 15:30:07 +0200

    frontend

---
+++ SRC/containers/HomePage/Apps.js
@@ -0,0 +1,8 @@
+import ".App.css";
+
+function App() {
+    return <div className="App">
+<HomePage />
+    </div>
+}
+export default App;
---
+++ SRC/containers/HomePage/topSection.jsx
@@ -0,0 +1,7 @@
+import React from "react";
+ HomePage(props) ; 
+ {
+     return <Pagecontainer>
+       Hello  
+     </Pagecontainer>
+ }
